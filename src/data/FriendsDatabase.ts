e66bce62f4af0ae67ff9b70f081da8bd9508f90b
Jerry Jones <jerryjfernandes@gmail.com>
2020-07-21 14:09:52 -0300

    Endpoint desfazer amizade

--- src/data/FriendsDatabase.ts
+++ src/data/FriendsDatabase.ts
@@ -11,7 +11,22 @@ export class FriendsDatabase extends BaseDatabase {
                 user_id,
                 user_to_follow_id
             })
-            .into(process.env.FRIENDSHIP_DB_NAM);
+            .into(process.env.FRIENDSHIP_DB_NAME);
+        } catch(error) {
+            throw new Error(error.sqlMessage || error.message)
+        }
+    }
+
+    public async undo(
+        user_id: string,
+        user_to_unfollow_id: string
+    ): Promise<any> {
+        try {
+            return await this.getConnection()
+            .delete()
+            .from(process.env.FRIENDSHIP_DB_NAME)
+            .where({user_id: user_id, user_to_follow_id: user_to_unfollow_id});
+
         } catch(error) {
             throw new Error(error.sqlMessage || error.message)
         }
